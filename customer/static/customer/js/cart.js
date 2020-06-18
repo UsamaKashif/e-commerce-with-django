9dbd4e3041f60a2ec3a77ebc717d94308fcfa109
UsamaKashif <49620321+UsamaKashif@users.noreply.github.com>
2020-06-18 17:21:39 +0500

    fix

--- customer/static/customer/js/cart.js
+++ customer/static/customer/js/cart.js
@@ -2,8 +2,8 @@ let updateBtns = document.getElementsByClassName('update-cart')
 
 for(i=0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
-        let productId = this.dataset.product 
-        let action = this.dataset.action 
+        let productId = this.dataset.product
+        let action = this.dataset.action
         console.log("productID:",productId, 'Action:',action)
 
         // console.log(user)
